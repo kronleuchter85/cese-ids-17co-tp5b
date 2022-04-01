35e5a47f2fd696adaac8adaa21d13b1ee6650799
kronleuchter85 <kronleuchter85@gmail.com>
2022-04-01 01:39:47 +0100

    Se agregan los datos del alumno Carreno, Gonzalo

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s GONZALO_CARRENO = {
+    .apellidos = "CARRENO",
+    .nombres = "Gonzalo Carreno",
+    .documento = "31.789.456",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &GONZALO_CARRENO,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
